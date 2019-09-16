4cb18d2e676f2947c94d525ddda75327cec6fa7f
Angelos Tsiaras <aggelostsiaras@gmail.com>
2019-09-16 08:44:47 +0200

    no message

--- hops/__main__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-if __name__ == '__main__':
-    import hops
-    hops.run_app()
--- windows_installer.cmd
+++
@@ -1,7 +0,0 @@
-SET mydir=%cd%
-
-python "%mydir%\setup.py" install
-
-copy "%mydir%\setup2.py" "%HOMEPATH%\setup2.py"
-python "%HOMEPATH%\setup2.py"
-del "%HOMEPATH%\setup2.py"
