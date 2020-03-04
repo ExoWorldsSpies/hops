586b9fdcd5c4be73b4c6f86366a8e157097bb11b
Angelos Tsiaras <aggelostsiaras@gmail.com>
2020-03-04 14:42:02 +0000

    print comment when desktop not found

--- setup2.py
+++ setup2.py
@@ -14,19 +14,22 @@ name = 'hops'
 app_dir = __get_abspath__()
 system = platform.system()
 
-excecutable = {'Darwin': 'command', 'Linux': 'sh', 'Windows': 'cmd'}
+executable = {'Darwin': 'command', 'Linux': 'sh', 'Windows': 'cmd'}
 
 # create shortcut
 try:
-    shortcut = os.path.join(os.path.expanduser('~'), 'Desktop', name + '.' + excecutable[system])
+    shortcut = os.path.join(os.path.expanduser('~'), 'Desktop2', name + '.' + executable[system])
     w = open(shortcut, 'w')
     w.write('python \"' + app_dir + '\"')
     w.close()
 except IOError:
-    shortcut = os.path.join(os.path.expanduser('~'), name + '.' + excecutable[system])
+    shortcut = os.path.join(os.path.expanduser('~'), name + '.' + executable[system])
     w = open(shortcut, 'w')
     w.write('python \"' + app_dir + '\"')
     w.close()
+    print('\n\n************\nCould not find your Desktop. The shortcut is saved in your home folder:\n\n{0}\n\n'
+          'You can freely move this file to your preferred location.\n************\n\n'.format(shortcut))
+    x = input('Press enter to complete installation.\n')
 
 if system == 'Darwin':
     os.system('chmod 755 ' + shortcut)
