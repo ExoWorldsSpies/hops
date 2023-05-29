9b51f022b9e58711ed9d40f37f0afc9901b68de7
Angelos Tsiaras <aggelostsiaras@gmail.com>
2023-05-29 16:37:33 +0200

    Version control for twirl

--- hops/__init__.py
+++ hops/__init__.py
@@ -1,6 +1,6 @@
 
-__version__ = '3.1.1'
-__message__ = 'Version 3.1 is now online with many new features!\n3.1.1 - Many bugs fixed, improved alignment, option to load location from profile, option to crop image edges.'
+__version__ = '3.1.2'
+__message__ = 'Version 3.1 is now online with many new features!\n3.1.1 - Many bugs fixed, improved alignment, option to load location from profile, option to crop image edges.\n3.1.2 - Version control for twirl'
 
 from .__run__ import run_app
 
