f126dc02e52800875b94bc6edea04bf1e75bf7a9
Angelos Tsiaras <aggelostsiaras@gmail.com>
2024-04-19 16:02:33 +0300

    Update installer_windows.cmd

--- installer_windows.cmd
+++ installer_windows.cmd
@@ -7,6 +7,6 @@ SET /p pydir=<pydir.txt
 call "%pydir%"
 
 python -m pip install -r "%mydir%\requirements.txt"
-python -m pip install "$mydir"
+python -m pip install "%mydir%"
 python "%mydir%\post_setup.py"
 python "%mydir%\setup_close.py"
