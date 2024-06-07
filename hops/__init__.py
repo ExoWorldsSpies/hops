f9d7b38e49e4002dfdce11ed3cdff20d8c87307a
Angelos Tsiaras <aggelostsiaras@gmail.com>
2024-06-07 16:20:07 +0300

    Update __init__.py

--- hops/__init__.py
+++ hops/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = '3.2.3'
+__version__ = '3.2.4'
 __message__ = 'Version 3.2 is now online with new features, including connection with GAIA DR3!\nv3.2.1 - Avoid bright stars in alignment, version control for reduction.\nv3.2.2 - Fix GAIA catalogue bug.v3.2.3 - Fix installation overwriting older versions.v3.2.4 - Matplotlib v3.9.0 bug fixed.'
 
 from .__run__ import run_app
